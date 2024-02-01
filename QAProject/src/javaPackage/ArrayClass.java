4e24a40319a3d542a4638fa21d339b73ce78185a
sudhu <sudhu@KuttyHP.lan>
2024-01-31 23:49:02 -0500

    Project Upload

---
+++ QAProject/src/javaPackage/ArrayClass.java
@@ -0,0 +1,19 @@
+package javaPackage;
+
+public class ArrayClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] a= {10,20,30,40,50};
+		
+		System.out.println(a.length);
+		System.out.println(a[01]);
+		
+		for(int i =0;i<=a.length;i++)
+		{
+			System.out.println(a[i]);
+		}
+	}
+
+}
